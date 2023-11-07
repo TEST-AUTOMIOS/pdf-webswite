dfa5171dc90eae3fd42b952e7d7b5d50fcb42452
Dinesh8056208 <testautomios@gmail.com>
2023-11-07 12:25:59 +0530

    ui
    responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/Footer.css";
 import { Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-function Footer() {
+
+
+
+function Footer(){
+
+
   return (
     <Container fluid className="fotter">
       <div className="fotter-di">
@@ -11,7 +17,7 @@ function Footer() {
             <div className="fotter-column">
               <ul>
                 <p>RedoPdf</p>
-                <li>Home</li>
+                <li >Home</li>
                 <li>Features</li>
                 <li>Pricing</li>
                 <li>Tools</li>
@@ -130,14 +136,16 @@ function Footer() {
         English
         </Col>
         <Col lg="4"md="6" sm="6" xs="8">
-        @RedoPdf-2023 
-        <svg
+        @RedoPdf-2023
+        <span> </span> &nbsp;
+        <svg style={{backgroundColor:'white'}}
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="black"
                   class="bi bi-twitter-x "
                   viewBox="0 0 16 16"
+                
                 >
                   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
                 </svg>{" "}
